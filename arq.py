a3f45e2a68df906cf95f5bea7d1ef49401fcca11
Rodrigo de Bona Sartor <rodrigo.goblin@gmail.com>
2018-08-18 11:29:07 -0300

    Leitura de arquivo JSON

--- arq.py
+++ arq.py
@@ -1,4 +1,5 @@
 from csv import DictReader
+import json
 
 class ArquivoCsv:
     def __init__(self, arquivo):
@@ -15,4 +16,24 @@ class ArquivoCsv:
                     dados.append(linha)
         except FileNotFoundError:
             self.existe = False
+        return dados
+
+class ArquivoJson:
+    def __init__(self, arquivo):
+        self.arquivo = arquivo
+        self.existe = None
+        self.valido = None
+    
+    def ler(self):
+        dados = []
+        try:
+            with open(self.arquivo, encoding='utf-8') as arq:
+                self.existe = True
+                dados = json.load(arq)
+                self.valido = True
+        except FileNotFoundError:
+            self.existe = False
+            self.valido = False
+        except json.decoder.JSONDecodeError:
+            self.valido = False
         return dados
--- test_arq.py
+++ test_arq.py
@@ -1,5 +1,5 @@
 import unittest
-from arq import ArquivoCsv
+from arq import ArquivoCsv, ArquivoJson
 
 class TestArquivoCsv(unittest.TestCase):
     def test_init(self):
@@ -33,6 +33,41 @@ class TestArquivoCsv(unittest.TestCase):
         arquivoCsv = ArquivoCsv('vazio.txt')
         dados = arquivoCsv.ler()
         self.assertEqual(arquivoCsv.existe, True)
+        self.assertEqual(len(dados), 0)
+
+class TestArquivoJson(unittest.TestCase):
+    def test_init(self):
+        arquivoJson = ArquivoJson('conteudo.json')
+        self.assertEqual(arquivoJson.arquivo, 'conteudo.json')
+        self.assertEqual(arquivoJson.existe, None)
+
+    def test_ler_arquivo_valido(self):
+        arquivoJson = ArquivoJson('conteudo.json')
+        dados = arquivoJson.ler()
+        self.assertEqual(arquivoJson.existe, True)
+        self.assertEqual(len(dados), 14)
+        
+        primeiraLinha = dados[0]
+        self.assertEqual(primeiraLinha['nome'], 'Diego Faria')
+        self.assertEqual(primeiraLinha['cidade'], 'Tubarão')
+        self.assertEqual(primeiraLinha['estado'], 'SC')
+
+        ultimaLinha = dados[-1]
+        self.assertEqual(ultimaLinha['nome'], 'Cléber Warmling')
+        self.assertEqual(ultimaLinha['cidade'], 'São Paulo')
+        self.assertEqual(ultimaLinha['estado'], 'SP')
+
+    def test_ler_arquivo_que_nao_existe(self):
+        arquivoJson = ArquivoJson('conteudox.json')
+        dados = arquivoJson.ler()
+        self.assertEqual(arquivoJson.existe, False)
+        self.assertEqual(len(dados), 0)
+
+    def test_ler_arquivo_vazio(self):
+        arquivoJson = ArquivoJson('vazio.txt')
+        dados = arquivoJson.ler()
+        self.assertEqual(arquivoJson.existe, True)
+        self.assertEqual(arquivoJson.valido, False)
         self.assertEqual(len(dados), 0)
 
 if __name__ == '__main__':
